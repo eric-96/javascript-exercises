9893df01f54a012585408e4b09246e06888e44a2
Eric <ericpossollo@gmail.com>
2023-02-19 23:47:05 +0000

    #

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+//1st
+const repeatString = function(str, number) {
+    
+    let theWord = ''
+    let errorMsg = 'ERROR'
 
-};
+    if (number < 0){
+    return errorMsg;
+    }
+    for (let i = 0 ; i < number; i++) {
+        theWord += str;
+    }   
+    return theWord;
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    if (typeof start !== 'number' || typeof end !== 'number'){
+        return 'ERROR';
+    }
+    else if (start < 0 || end < 0){
+        return 'ERROR';
+    }
 
-};
+    const min = Math.min(start, end);
+    const max = Math.max(start, end);
+    
+    let sum = 0;
+        for (let i = min; i <= max; i++) {
+        sum += i;
+        }
+            return sum;
+    }
 
+console.log(sumAll(5000, 2))
 // Do not edit below this line
 module.exports = sumAll;
